b0e7a80387d2c7ae25e182a74dbd62cf30fa9088
Jane Doe <ktrnaveen91@gmail.com>
2025-01-07 12:52:27 +0530

    Card hover style

--- tile-test-app/src/app/shared/card/styles.ts
+++ tile-test-app/src/app/shared/card/styles.ts
@@ -3,17 +3,33 @@ import { Card, CardContent, CardContentProps, CardMedia, CardMediaProps, CardPro
 
 export const StyledCard = styled(Card)<CardProps>({
     backgroundColor: '#fff',
-    padding:"20px",
+    padding:"3.125rem",
+    borderRadius: '.5rem',
+    boxShadow: '0 0 #000, 0 0 #000, 0px 6px 90px rgba(8,14,40,.04)',
+    transitionProperty: "all",
+    transitionTimingFunction: "cubic-bezier(.4,0,.2,1)",
+    transitionDuration: ".15s",
+    "&:hover": {
+        boxShadow: '0 0 #000, 0 0 #000, 0px 6px 90px rgba(8, 14, 40, .1)',
+    }
 });
 export const StyledCardMedia = styled(CardMedia)<CardMediaProps>({
     width: '60px',
 });
 export const StyledCardContent = styled(CardContent)<CardContentProps>({
     padding:"10px 0px 0px",
+   "p":{
+        color: 'rgb(117 118 147)',
+        fontSize: "16px",
+        lineHeight: "26px",
+        fontWeight: "400",
+    },
     "&:last-child": {
         padding:"10px 0px 0px",
     }
 });
 export const CardContentHeading = styled(Typography)<TypographyProps>({
-    fontSize: '18px',
+    fontWeight: "700",
+    fontSize: "26px",
+    lineHeight: "32px",
 });
